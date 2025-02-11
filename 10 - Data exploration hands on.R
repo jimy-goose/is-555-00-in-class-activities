0a14e0e664964371c11387bed27770e75c777482
jimy-goose <131339722+jimy-goose@users.noreply.github.com>
2025-02-11 09:52:56 -0700

    Idk if this worked last time

--- 10 - Data exploration hands on.R
+++ 10 - Data exploration hands on.R
@@ -5,12 +5,35 @@ df2 <- read_csv('https://www.dropbox.com/s/xfp1qzxvo19ym0x/df_2.csv?dl=1')
 df3 <- read_csv('https://www.dropbox.com/s/uzusr9723ffn546/df_3.csv?dl=1')
 df4 <- read_csv('https://www.dropbox.com/s/js8tehtsk7btpeq/df_4.csv?dl=1')
 
+df1
+
+df2
+
+df3
+
+df4
+
 # A few sample calculations:
 df1 %>% summarize(across(everything(), list(mean = ~mean(.x), 
                                             sd = ~sd(.x))))
+df2 %>% summarize(across(everything(), list(mean = ~mean(.x), 
+                                            sd = ~sd(.x))))
+df3 %>% summarize(across(everything(), list(mean = ~mean(.x), 
+                                            sd = ~sd(.x))))
+df4 %>% summarize(across(everything(), list(mean = ~mean(.x), 
+                                            sd = ~sd(.x))))
+
+
+
 
 # correlation matrix:
 df1 %>% cor()
+
+df2 %>% cor()
+
+df3 %>% cor()
+
+df4 %>% cor()
 
 # simple regression analysis...but here's an explanation:
 # 
@@ -18,3 +41,9 @@ df1 %>% cor()
 # y ~ x --> uses R's formula expression syntax to say "y regressed on x"
 # %>% coefficients() --> this just extracts the coefficients from the resulting model
 lm(y ~ x, data = df1) %>% coefficients()
+
+lm(y ~ x, data = df2) %>% coefficients()
+
+lm(y ~ x, data = df3) %>% coefficients()
+
+lm(y ~ x, data = df4) %>% coefficients()
